d2862ba69cb1a726ba68dff222d09947f7d8d5af
aixiaoqie <2390734879@qq.com>
2019-11-26 13:49:58 +0800

    引用IDWorker

--- tensquare-parent/tensquare-base/src/main/java/com/ssw/base/BaseApplication.java
+++ tensquare-parent/tensquare-base/src/main/java/com/ssw/base/BaseApplication.java
@@ -1,7 +1,9 @@
 package com.ssw.base;
 
+import com.ssw.util.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
@@ -9,5 +11,9 @@ import org.springframework.context.annotation.Configuration;
 public class BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class,args);
+    }
+    @Bean
+    public IdWorker generateId(){
+        return new IdWorker();
     }
 }
