fc3d3a4895294116a640f8acb2e699ea046b58d9
aixiaoqie <2390734879@qq.com>
2019-11-30 20:46:16 +0800

    实现label的条件查询以及分页条件查询

--- tensquare-parent/tensquare-base/src/main/java/com/ssw/base/controller/LabelController.java
+++ tensquare-parent/tensquare-base/src/main/java/com/ssw/base/controller/LabelController.java
@@ -2,12 +2,15 @@ package com.ssw.base.controller;
 
 import com.ssw.base.pojo.Label;
 import com.ssw.base.service.LabelService;
+import com.ssw.entity.PageResult;
 import com.ssw.entity.ResultModel;
 import com.ssw.entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -19,32 +22,46 @@ public class LabelController {
     private LabelService labelService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResultModel findAll(){
+    public ResultModel findAll() {
         List<Label> labelList = labelService.findAll();
-        return new ResultModel(true, StatusCode.OK,"查询成功",labelList);
+        return new ResultModel(true, StatusCode.OK, "查询成功", labelList);
     }
+
     @RequestMapping(method = RequestMethod.POST)
-    public ResultModel save(@RequestBody Label label){
+    public ResultModel save(@RequestBody Label label) {
         labelService.save(label);
-        return new ResultModel(true,StatusCode.OK,"保存成功");
+        return new ResultModel(true, StatusCode.OK, "保存成功");
     }
-    @RequestMapping(value = "/{labelId}",method = RequestMethod.GET)
-    public ResultModel findById(@PathVariable("labelId") String labelId){
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.GET)
+    public ResultModel findById(@PathVariable("labelId") String labelId) {
         Label label = labelService.findById(labelId);
-        return new ResultModel(true, StatusCode.OK,"查询成功",label);
+        return new ResultModel(true, StatusCode.OK, "查询成功", label);
     }
-    @RequestMapping(value = "/{labelId}",method = RequestMethod.PUT)
-    public ResultModel update(@PathVariable String labelId, @RequestBody Label label){
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.PUT)
+    public ResultModel update(@PathVariable String labelId, @RequestBody Label label) {
         label.setId(labelId);
         labelService.update(label);
-        return new ResultModel(true, StatusCode.OK,"修改成功");
+        return new ResultModel(true, StatusCode.OK, "修改成功");
     }
-    @RequestMapping(value = "/{labelId}",method = RequestMethod.DELETE)
-    public ResultModel delete(@PathVariable("labelId") String labelId){
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.DELETE)
+    public ResultModel delete(@PathVariable("labelId") String labelId) {
         labelService.delete(labelId);
-        return new ResultModel(true, StatusCode.OK,"删除成功");
+        return new ResultModel(true, StatusCode.OK, "删除成功");
     }
 
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public ResultModel search(@RequestBody Label label) {
+        List<Label> list = labelService.search(label);
+        return new ResultModel(true, StatusCode.OK, "查询成功", list);
+    }
 
+    @RequestMapping(value = "/search/{page}/{size}", method = RequestMethod.POST)
+    public ResultModel pageQuery(@PathVariable("page") int page, @PathVariable("size") int size, @RequestBody Label label) {
+        Page<Label> pageData = labelService.pageQuery(page, size, label);
+        return new ResultModel(true, StatusCode.OK, "查询成功", new PageResult<Label>(pageData.getTotalElements(), pageData.getContent()));
+    }
 
 }
--- tensquare-parent/tensquare-base/src/main/java/com/ssw/base/service/LabelService.java
+++ tensquare-parent/tensquare-base/src/main/java/com/ssw/base/service/LabelService.java
@@ -3,12 +3,21 @@ package com.ssw.base.service;
 import com.ssw.base.dao.LabelDao;
 import com.ssw.base.pojo.Label;
 import com.ssw.util.IdWorker;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -18,17 +27,18 @@ public class LabelService {
 
     @Autowired
     private IdWorker idWorker;
+
     public List<Label> findAll() {
-        return  labelDao.findAll();
+        return labelDao.findAll();
     }
 
     public void save(Label label) {
-        label.setId(idWorker.nextId()+"");
+        label.setId(idWorker.nextId() + "");
         labelDao.save(label);
     }
 
 
-    public Label findById(String labelId ) {
+    public Label findById(String labelId) {
         return labelDao.findById(labelId).get();
     }
 
@@ -38,5 +48,72 @@ public class LabelService {
 
     public void delete(String id) {
         labelDao.deleteById(id);
+    }
+
+    /**
+     * @desciption 条件查询
+     * @author ssw
+     * @date 2019/11/30
+     * @time 20:20
+     */
+    public List<Label> search(Label label) {
+        return labelDao.findAll(new Specification<Label>() {
+            /**
+             *
+             * @param root 根对象，将哪个对象封装进来
+             * @param criteriaQuery 封装查询关键字 例如 group by ，order by
+             * @param criteriaBuilder  用来封装查询条件 ，直接返回null，表示没有条件
+             * @return
+             */
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                ArrayList<Predicate> arrayList = new ArrayList<>();
+                if (Strings.isNotBlank(label.getLabelname())) {
+                    // where labelname like %{label.getLabelname}%
+                    Predicate predicate = criteriaBuilder.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
+                    arrayList.add(predicate);
+                }
+                if (Strings.isNotBlank(label.getState())) {
+                    Predicate predicate = criteriaBuilder.equal(root.get("state").as(String.class), label.getState()); //where state = {label.getState}
+                    arrayList.add(predicate);
+                }
+
+                Predicate[] predicates = new Predicate[arrayList.size()];
+                arrayList.toArray(predicates);
+                return criteriaBuilder.and(predicates);  //where labelname like %{label.getLabelname}% and tate = {label.getState}
+            }
+        });
+    }
+
+    /**
+     * 分页条件查询
+     *
+     * @param page  页码
+     * @param size  每页数据条数
+     * @param label 分页查询对象
+     * @date 2019/11/30
+     * @time 20:20
+     */
+    public Page<Label> pageQuery(int page, int size, Label label) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return labelDao.findAll(new Specification<Label>() {
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                ArrayList<Predicate> arrayList = new ArrayList<>();
+                if (Strings.isNotBlank(label.getLabelname())) {
+                    // where labelname like %{label.getLabelname}%
+                    Predicate predicate = criteriaBuilder.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
+                    arrayList.add(predicate);
+                }
+                if (Strings.isNotBlank(label.getState())) {
+                    Predicate predicate = criteriaBuilder.equal(root.get("state").as(String.class), label.getState()); //where state = {label.getState}
+                    arrayList.add(predicate);
+                }
+
+                Predicate[] predicates = new Predicate[arrayList.size()];
+                arrayList.toArray(predicates);
+                return criteriaBuilder.and(predicates);  //where labelname like %{label.getLabelname}% and tate = {label.getState}
+            }
+        }, pageable);
     }
 }
