35abbb5f59c6af830cde2efcd8ea3a412183d268
aixiaoqie <2390734879@qq.com>
2019-12-05 23:57:51 +0800

    问答服务dao层开发

--- tensquare-parent/tensquare-qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
+++ tensquare-parent/tensquare-qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
@@ -1,14 +1,28 @@
 package com.tensquare.qa.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.tensquare.qa.pojo.Problem;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 /**
  * 数据访问接口
  * @author Administrator
  *
  */
 public interface ProblemDao extends JpaRepository<Problem,String>,JpaSpecificationExecutor<Problem>{
-	
+
+    @Query(value = "SELECT * FROM tb_problem,tb_pl WHERE id = problemid AND labelid = ? ORDER BY replytime DESC",nativeQuery = true)
+    public Page<Problem> newList(String lableId, Pageable pageable);
+
+    @Query(value = "",nativeQuery = true)
+    public Page<Problem> hotlist(String lableId, Pageable pageable);
+
+    @Query(value = "",nativeQuery = true)
+    public Page<Problem> waitlist(String lableId, Pageable pageable);
 }
