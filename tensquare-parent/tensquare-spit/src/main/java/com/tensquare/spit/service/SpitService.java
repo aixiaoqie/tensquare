f1d05b838bb4370a717890b5613b612ac0423c3e
aixiaoqie <2390734879@qq.com>
2019-12-23 00:13:15 +0800

    修改新增吐槽的业务逻辑，若有父节点则父节点回复数加1

--- tensquare-parent/tensquare-spit/src/main/java/com/tensquare/spit/service/SpitService.java
+++ tensquare-parent/tensquare-spit/src/main/java/com/tensquare/spit/service/SpitService.java
@@ -14,6 +14,7 @@ import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -41,6 +42,22 @@ public class SpitService {
 
     public void save(Spit spit) {
         spit.set_id(idWorker.nextId() + "");
+        //初始化数据完善
+        spit.setPublishtime(new Date());//发布日期
+        spit.setVisits(0);//浏览量
+        spit.setShare(0);//分享数
+        spit.setThumbup(0);//点赞数
+        spit.setComment(0);//回复数
+        spit.setState("1");//状态
+        //判断当前吐槽是否有父节点
+        if(spit.getParentid()!=null && !"".equals(spit.getParentid())){
+            //给父节点吐槽的回复数加一
+            Query query = new Query();
+            query.addCriteria(Criteria.where("_id").is(spit.getParentid()));
+            Update update = new Update();
+            update.inc("comment", 1);
+            mongoTemplate.updateFirst(query, update, "spit");
+        }
         spitDao.save(spit);
     }
 
