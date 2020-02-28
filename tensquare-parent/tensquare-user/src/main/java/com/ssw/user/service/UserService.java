2d99312a742a02c1140d5617a5234e4451957f80
aixiaoqie <2390734879@qq.com>
2020-02-28 22:51:38 +0800

    短信发送模块实现

--- tensquare-parent/tensquare-user/src/main/java/com/ssw/user/service/UserService.java
+++ tensquare-parent/tensquare-user/src/main/java/com/ssw/user/service/UserService.java
@@ -198,7 +198,7 @@ public class UserService {
 		Map<String, String> map = new HashMap<>();
 		map.put("mobile", mobile);
 		map.put("checkcode", checkcode);
-		//rabbitTemplate.convertAndSend("sms", map);
+		rabbitTemplate.convertAndSend("sms", map);
 		//在控制台显示一份【方便测试】
 		System.out.println("验证码为："+checkcode);
 	}
